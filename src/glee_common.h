f65f027998e848d8c0d3df560864ae27098fc663
LogicEu <eugenioarteagaa@gmail.com>
2021-08-21 18:25:47 -0400

    added guards to glee_common.h

--- src/glee_common.h
+++ src/glee_common.h
@@ -1,3 +1,6 @@
+#ifndef GLEE_COMMON_H
+#define GLEE_COMMON_H
+
 #include <glee.h>
 
 typedef struct gleeManager {
@@ -16,3 +19,5 @@ extern gleeManager glee;
 void GLmouse_scroll(GLFWwindow* window, double xoffset, double yoffset);
 void GLkeyboard(GLFWwindow* window, int key, int scancode, int action, int mods);
 void GLdrop_file(GLFWwindow* windwo, int count, const char** paths);
+
+#endif
